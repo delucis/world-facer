c263f17a048ec516e4de574eabb68b51d1d133a9
delucis <swithinbank@gmail.com>
2018-08-24 01:53:37 +0200

    test(paper-words): Make sure paper-scraper runs before testing paper-words

--- test/paper-words.js
+++ test/paper-words.js
@@ -1,7 +1,14 @@
-import test from 'ava'
+import ava from 'ava'
+import ninos from 'ninos'
+import ps from '../lib/paper-scraper'
 import pw from '../lib/paper-words'
 
+const test = ninos(ava)
+
 test('It returns a string that is very long', async t => {
+  console.log = t.context.stub()
+  console.error = t.context.stub()
+  await ps({ n: 3 })
   const words = await pw()
   t.is(typeof words, 'string')
 })
