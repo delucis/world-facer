d66993ddeddae953e1513d0e77d65cd9537f7cc6
delucis <swithinbank@gmail.com>
2018-09-08 00:35:49 -0400

    refactor(nlp + top-words): Move top-words function into nlp module
    BREAKING CHANGE: `lib/top-words` provided a way to get the raw top words in a string before
    `lib/nlp` had been written. Given its similarity to the other language tools, `lib/top-words` is now
    available as the `topwords` method in `lib/nlp`.

--- test/nlp.js
+++ test/nlp.js
@@ -19,3 +19,45 @@ test('Collects key words from input text', async t => {
   t.true(Array.isArray(words))
   words.forEach(phrase => t.is(typeof phrase, 'string'))
 })
+
+test('topwords > It throws if passed a non-string argument', t => {
+  t.throws(() => { nlp.topwords(75) })
+})
+
+test('topwords > It throws if passed badly formed options', t => {
+  t.throws(() => { nlp.topwords('string', { n: 'three', lang: 57 }) })
+})
+
+test('topwords > Identifies the most common word', t => {
+  const s = 'A sentence with the word sentence repeated.'
+  t.is(nlp.topwords(s)[0], 'sentence')
+})
+
+test('topwords > Funktioniert auch auf Deutsch', t => {
+  const s = 'Satz für Satz wird der Dichter satt.'
+  t.is(nlp.topwords(s, { lang: 'de' })[0], 'satz')
+})
+
+test('topwords > Ignores imaginary or unknown languages', t => {
+  const s = 'A sentence with the word sentence repeated.'
+  t.is(nlp.topwords(s, { lang: 'toffee' })[0], 'sentence')
+})
+
+test('topwords > Ignores punctuation', t => {
+  const s = 'A sentence with repeated words excludes punctuation, lumping together words followed or preceded by punctuation when they are repeated. This is what it means to be repeated: “repeated” words can be simply repeated, reiterated or emphasised.'
+  t.is(nlp.topwords(s)[0], 'repeated')
+})
+
+test('topwords > Lets you specify the number of results', t => {
+  const s = 'Some long sentence with plenty of words in to analyse and only extract the top three words from even though there are more than three words.'
+  t.is(nlp.topwords(s, { n: 3 }).length, 3)
+})
+
+test('topwords > Can’t return more words than are input', t => {
+  t.is(nlp.topwords('Short sentence', { n: 5 }).length, 2)
+})
+
+test('topwords > Can return a 2-tuple of [word, count] if asked', t => {
+  const s = 'A sentence with the word sentence repeated.'
+  t.deepEqual(nlp.topwords(s, { includeCount: true })[0], ['sentence', 2])
+})
