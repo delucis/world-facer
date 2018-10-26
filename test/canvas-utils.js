5858bf012e4985146d140ed6badc142c34b365dd
delucis <swithinbank@gmail.com>
2018-10-26 15:58:40 -0400

    fix(canvas-utils): Rename crop to truncate

--- test/canvas-utils.js
+++ test/canvas-utils.js
@@ -1,16 +1,16 @@
 import test from 'ava'
-import { crop, fit } from '../lib/canvas-utils'
+import { truncate, fit } from '../lib/canvas-utils'
 
-test('crop: returns input if they match aspect ratio', t => {
-  t.deepEqual(crop([200, 100], 2), [200, 100])
+test('truncate: returns input if they match aspect ratio', t => {
+  t.deepEqual(truncate([200, 100], 2), [200, 100])
 })
 
-test('crop: scales height if aspect less than desired', t => {
-  t.deepEqual(crop([100, 100], 2), [100, 50])
+test('truncate: scales height if aspect less than desired', t => {
+  t.deepEqual(truncate([100, 100], 2), [100, 50])
 })
 
-test('crop: scales width if aspect greater than desired', t => {
-  t.deepEqual(crop([300, 100], 2), [200, 100])
+test('truncate: scales width if aspect greater than desired', t => {
+  t.deepEqual(truncate([300, 100], 2), [200, 100])
 })
 
 test('fit: returns input if input fits', t => {
